a60f9157f3784146e9ccdce7198509da854688fb
Lars Bohm <larsbohm@gmail.com>
2023-01-19 11:00:44 +0100

    basic route/controller setup, forms, meta setup

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem "bootsnap", require: false
 
 # CUSTOM GEMS
 gem 'devise'
+gem 'simple_form'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/controllers/suppliers/supplier_locations_controller.rb
+++ app/controllers/suppliers/supplier_locations_controller.rb
@@ -1,30 +1,50 @@
-class SupplierLocations < ApplicationController
+class Suppliers::SupplierLocationsController < ApplicationController
   before_action :set_supplier_location, only: [:show, :edit, :update, :destroy]
 
   def dashboard
+    @supplier_location = current_supplier.supplier_locations
   end
 
   def show
   end
 
   def new
+    @supplier_location = SupplierLocation.new
   end
 
   def create
+    @supplier_location = SupplierLocation.new(supplier_location_params)
+    @supplier_location.supplier = current_supplier
+    if @supplier_location.save
+      suppliers_supplier_location_path(@supplier_location)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @supplier_location.update(supplier_location_params)
+      suppliers_supplier_location_path(@supplier_location)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if @supplier_location.destroy
+      suppliers_path
+    else
+      suppliers_path
+    end
   end
 
   private
 
   def set_supplier_location
+    @supplier_location = SupplierLocation.find(params[:id])
   end
 
   def supplier_location_params

ac3d982e8743c9f6c516776ef521c94c077dfa7d
michel van dijck <michvd92@gmail.com>
2023-01-20 09:29:50 +0100

    add dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,22 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 
-# suplier = Supplier.create(name: 'Test', email: 'test@niw.com', password: "test123")
+supplier = Supplier.new(name: "NIW", email:"test@niw.com", password:"test123")
+
+filepath = './data/producer_data.csv'
+CSV.foreach(filepath) do |row|
+  name = row[1]
+  lon = row[4]
+  lat = row[5]
+
+  supplier_location = SuppierLocation.new(
+    name: name
+    longitude: lon
+    latitude: lat
+  )
+  supplier_location.supplier = suplier
+  supplier_location.save
+end
 
 
-# CSV.foreach(filepath) do |row|
-#   name = row[0]
-#   supplier_location = SuppierLocation.new(
-#     name: name
-#   )
-#   supplier_location.supplier = suplier
-#   supplier_location.save
-# end
-
-supplier = Supplier.new(name: "Lars")
-
-p supplier
+end

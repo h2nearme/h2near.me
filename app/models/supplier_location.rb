e146419602745aac3fa7fc70ef8104aa5a1f099c
Lars Bohm <larsbohm@gmail.com>
2023-01-20 15:49:47 +0100

    add address to supplier locationg

--- app/models/supplier_location.rb
+++ app/models/supplier_location.rb
@@ -3,6 +3,10 @@ class SupplierLocation < ApplicationRecord
 
   after_save :set_prefixed_id
 
+  def address
+    "#{self.house_nr} - #{self.postal_code}"
+  end
+
   def set_prefixed_id
     if self.prefixed_id.nil?
       self.prefixed_id = "s_#{self.id}"

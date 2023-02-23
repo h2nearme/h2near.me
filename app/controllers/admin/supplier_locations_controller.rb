729c2db026f3542d64ca59d31b1943fe90ac2bc2
Lars Bohm <larsbohm@gmail.com>
2023-02-23 13:39:30 +0100

    fix search filter on admin dashboard

--- app/controllers/admin/supplier_locations_controller.rb
+++ app/controllers/admin/supplier_locations_controller.rb
@@ -1,6 +1,19 @@
 class Admin::SupplierLocationsController < Admin::BaseController
   def index
-    @supplier_locations = SupplierLocation.all.order('updated_at DESC').paginate(page: params[:page], per_page: 10)
+    if params[:q]
+      @supplier_locations = SupplierLocation.where("name ILIKE ?", "%#{params[:q]}%").order('updated_at DESC').paginate(page: params[:page], per_page: 10)
+
+    else
+      @supplier_locations = SupplierLocation.all.order('updated_at DESC').paginate(page: params[:page], per_page: 10)
+    end
+    respond_to do |format|
+      format.html
+      format.json {
+          render json: {
+              locations: render_to_string(partial: "admin/supplier_locations/locations", formats: [:html]), 
+          }
+      }
+    end
   end
 
   def verify

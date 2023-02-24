f23e2f30b0c83853d81058ab3ea45c26b85cc4fd
Lars Bohm <larsbohm@gmail.com>
2023-02-24 09:10:42 +0100

    change application password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "volta", password: "s72"
+  http_basic_authenticate_with name: "niw", password: "kier"
 
 end
